e6fbcd4ad37ec96e0435d16e22b852e4731ca945
pan-yuhan <1352164869@qq.com>
2022-11-23 20:45:07 +0800

    Initial commit

---
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -0,0 +1,18 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+
+    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit); //给多个参量所以是动态sql，offset是起始行号，limit数据长度
+
+    // @Param注解用于给参数取别名,
+    // 如果只有一个参数,并且在<if>里使用,则必须加别名.
+    int selectDiscussPostRows(@Param("userId") int userId);
+
+}
